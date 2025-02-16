59d4b0b27aaccae0dd16d52a5af59d26202f2acb
pedro.barboza <pedro123barboza@gmail.com>
2025-02-15 22:44:19 -0300

    feat: I created a controller and a route to the controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', [MainController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
