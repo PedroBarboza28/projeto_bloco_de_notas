292814ab2f2ec571cd02b3ad154e85bf6f3089ca
pedro.barboza <pedro123barboza@gmail.com>
2025-02-16 02:14:51 -0300

    feat: I created the application's base layout and login form

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/login", [AuthController::class, "login"]);
+Route::get("/login", [AuthController::class, "login"])->name('login');  // Defina a rota nomeada
 
 Route::get("/logout", [AuthController::class, "logout"]);
