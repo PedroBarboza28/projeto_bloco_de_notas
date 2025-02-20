59f1bc76e0058667a5b65ed46c5db2190ff00698
pedro.barboza <pedro123barboza@gmail.com>
2025-02-20 00:34:58 -0300

    feat: Additional Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,19 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\checkIsLogged;
+use App\Http\Middleware\checkIsNotLogged;
 use Illuminate\Support\Facades\Route;
+
+// auth routes - user not logged
+Route::middleware([checkIsNotLogged::class])->group(function () {
 
 // auth routes
 Route::get("/login", [AuthController::class, "login"])->name('login');  // Defina a rota nomeada
 Route::post("/loginSubmit", [AuthController::class, "loginSubmit"]);
 
+});
+
+// auth routes - user logged
 Route::middleware([checkIsLogged::class])->group(function () {
 
     Route::get("/", [MainController::class, "index"]);
