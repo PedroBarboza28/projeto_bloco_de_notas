d21bb9c8f381cf9749cfca5f7f3615bd348f9da7
pedro.barboza <pedro123barboza@gmail.com>
2025-03-08 15:37:53 -0300

    feat: Carregando As Notas do User Logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 
 class MainController extends Controller
 {
     public function index()
     {
+
+        //load user routes
+        $id = session('user.id');
+        $user = User::find($id)->toArray();
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        echo'<pre>';
+        print_r($user);
+        print_r($notes);
+
+        die();
+        
         //load user routes
         return view('home');
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,4 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 //ele cria a tabela no plural e se conecta com o banco
 class User extends Model
 {
+    public function notes(){
+        return $this->hasMany(Note::class);
+    }
 }
