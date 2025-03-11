55229cbe553144733494e7ee3565707f0a58a9f4
pedro.barboza <pedro123barboza@gmail.com>
2025-03-10 22:13:26 -0300

    feat:  Validação Do Formulário e Criação da Nova Nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Http\Request;
@@ -24,8 +25,38 @@ class MainController extends Controller
     }
 
     public function newNoteSubmit(Request $request){
-        echo "Criando nova nota";
+// validate request
+ // form validation
+ $request->validate([
+    'text_title' => 'required|min:3|max:200',
+    'text_note' => 'required|min:3|max:3000',
+ ],
+ // error messages
+[
+    'text_title.required' => 'O título é obrigatório',
+    'text_title.min' => 'O título deve ter pelo menos :min caracteres',
+    'text_title.max' => 'O título deve ter no máximo :max caracteres',
 
+    'text_note.required' => 'A nota é obrigatória',
+    'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+    'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+ ]
+);
+
+// get user id
+
+$id = session('user.id');
+
+// create new note
+
+$note = new Note();
+$note->user_id = $id;
+$note->title = $request->text_title;
+$note->text = $request->text_note;
+$note->save();
+
+// redirect to home
+return redirect()->route('home');
     }
 
     public function editNote($id){
